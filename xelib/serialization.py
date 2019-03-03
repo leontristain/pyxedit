5a459a7bf5ecb61474fdfccefae9c36d55aba88f
leontristain <ltristain@yahoo.com>
2019-03-03 15:32:42 -0800

    implemented serialization.py

--- xelib/serialization.py
+++ xelib/serialization.py
@@ -1,15 +1,25 @@
+import json
+
+from xelib.lib import raw_api
+from xelib.helpers import get_string, validate
+from xelib.elements import element_context
+
 
 def element_to_json(id_):
-    raise NotImplementedError
+    return get_string(lambda len_: raw_api.ElementToJson(id_, len_),
+                      error_msg=f'Failed to serialize element to JSON: '
+                                f'{element_context(id_)}')
 
 
-def element_to_object(id_):
-    raise NotImplementedError
+def element_to_dict(id_):
+    return json.loads(element_to_json(id_))
 
 
 def element_from_json(id_, path, json):
-    raise NotImplementedError
+    validate(raw_api.ElementFromJson(id_, path, json),
+             f'Failed to deserialize element from JSON: '
+             f'{element_context(id_, path)}')
 
 
-def element_from_object(id_, path, obj):
-    raise NotImplementedError
+def element_from_dict(id_, path, dict_):
+    element_from_json(id_, path, json.dumps(dict_))
