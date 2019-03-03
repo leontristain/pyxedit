409cfc82b14c468b0f33162dbbfb0cb0647648ee
leontristain <ltristain@yahoo.com>
2019-03-03 15:52:33 -0800

    implemented all of files.py

--- xelib/files.py
+++ xelib/files.py
@@ -1,4 +1,8 @@
-from xelib.elements import (get_uint_value,
+from xelib.lib import raw_api
+from xelib.helpers import get_handle, validate, get_integer, get_string
+from xelib.elements import (element_context,
+                            get_element,
+                            get_uint_value,
                             set_uint_value,
                             name,
                             get_value,
@@ -61,61 +65,81 @@ def set_is_esm(id_, state):
 
 
 def add_file(file_name):
-    raise NotImplementedError
+    return get_handle(lambda res: raw_api.AddFile(file_name, res),
+                      error_msg=f'Failed to add new file {file_name}')
 
 
 def file_by_index(index):
-    raise NotImplementedError
+    return get_handle(lambda res: raw_api.FileByIndex(index, res))
 
 
 def file_by_load_order(load_order):
-    raise NotImplementedError
+    return get_handle(lambda res: raw_api.FileByLoadOrder(load_order, res))
 
 
 def file_by_name(file_name):
-    raise NotImplementedError
+    return get_handle(lambda res: raw_api.FileByName(file_name, res))
 
 
-def file_by_author(file_name):
-    raise NotImplementedError
+def file_by_author(author):
+    return get_handle(lambda res: raw_api.FileByAuthor(author, res))
 
 
 def nuke_file(id_):
-    raise NotImplementedError
+    validate(raw_api.NukeFile(id_), f'Failed to nuke file: {id_}')
 
 
 def rename_file(id_, new_file_name):
-    raise NotImplementedError
+    validate(raw_api.RenameFile(id_, new_file_name),
+             f'Failed to rename file {element_context(id_)} to {new_file_name}')
 
 
 def save_file(id_, file_path):
-    raise NotImplementedError
+    validate(raw_api.SaveFile(id_, file_path),
+             f'Failed to save file {element_context(id_)}')
 
 
 def get_record_count(id_):
-    raise NotImplementedError
+    return get_integer(
+        lambda res: raw_api.GetRecordCount(id_, res),
+        error_msg=f'Failed to get record count for {element_context(id_)}')
 
 
 def get_override_record_count(id_):
-    raise NotImplementedError
+    return get_integer(
+        lambda res: raw_api.GetOverrideRecordCount(id_, res),
+        error_msg=f'Failed to get override record count for '
+                  f'{element_context(id_)}')
 
 
 def md5_hash(id_):
-    raise NotImplementedError
+    return get_string(
+        lambda len_: raw_api.MD5Hash(id_, len_),
+        error_msg=f'Failed to get MD5 Hash for {element_context(id_)}')
+
+
+def crc_hash(id_):
+    return get_string(
+        lambda len_: raw_api.CRCHash(id_, len_),
+        error_msg=f'Failed to get CRC Hash for {element_context(id_)}')
 
 
 def get_file_load_order(id_):
-    raise NotImplementedError
+    return get_integer(
+        lambda res: raw_api.GetFileLoadOrder(id_, res),
+        error_msg=f'Failed to load order for ${element_context(id_)}')
 
 
 def get_file_header(id_):
-    raise NotImplementedError
+    return get_element(id_, 'File Header')
 
 
 def sort_editor_ids(id_, sig):
-    raise NotImplementedError
+    validate(raw_api.SortEditorIDs(id_, sig),
+             f'Failed to sort {sig} EditorIDs for: {element_context(id_)}')
 
 
 def sort_names(id_, sig):
-    raise NotImplementedError
+    validate(raw_api.SortNames(id_, sig),
+             f'Failed to sort {sig} Names for {element_context(id_)}')
 
