8a8aa41e8b776fd2e1972a1d9483ac6fd47f1a12
leontristain <ltristain@yahoo.com>
2019-03-03 15:08:40 -0800

    implemented messages.py

--- xelib/messages.py
+++ xelib/messages.py
@@ -1,15 +1,21 @@
+from xelib.lib import raw_api
+from xelib.helpers import get_string
+
 
 def get_messages():
-    raise NotImplementedError
+    return get_string(lambda len_: raw_api.GetMessagesLength(len_),
+                      method=raw_api.GetMessages)
 
 
 def clear_messages():
-    raise NotImplementedError
+    raw_api.ClearMessages()
 
 
 def get_exception_message():
-    raise NotImplementedError
+    return get_string(lambda len_: raw_api.GetExceptionMessageLength(len_),
+                      method=raw_api.GetExceptionMessage)
 
 
 def get_exception_stack():
-    raise NotImplementedError
+    return get_string(lambda len_: raw_api.GetExceptionStackLength(len_),
+                      method=raw_api.GetExceptionStack)
