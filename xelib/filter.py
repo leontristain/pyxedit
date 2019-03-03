d9a8d868648ddace0457f5396f0fe1b0127a2920
leontristain <ltristain@yahoo.com>
2019-03-03 15:01:18 -0800

    implemented filter.py

--- xelib/filter.py
+++ xelib/filter.py
@@ -1,7 +1,11 @@
+from xelib.lib import raw_api
+from xelib.helpers import validate
+from xelib.elements import name
+
 
 def filter_record(id_):
-    raise NotImplementedError
+    validate(raw_api.FilterRecord(id_), f'Failed to filter record {name(id_)}')
 
 
 def reset_filter():
-    raise NotImplementedError
+    validate(raw_api.ResetFilter(), f'Failed to reset filter')
