561c226c46ec7ae3bd2d20f30b5cb23a51f25c07
leontristain <ltristain@yahoo.com>
2019-03-03 15:02:22 -0800

    implemented groups.py

--- xelib/groups.py
+++ xelib/groups.py
@@ -1,11 +1,13 @@
+from xelib.elements import has_element, get_element, add_element
+
 
 def has_group(id_, signature):
-    raise NotImplementedError
+    return has_element(id_, signature)
 
 
 def add_group(id_, signature):
-    raise NotImplementedError
+    return add_element(id_, signature)
 
 
 def get_child_group(id_):
-    raise NotImplementedError
+    return get_element(id_, 'Child Group')
