90dc82409448641307fdd6a0e86eb07a5f38e60c
leontristain <ltristain@yahoo.com>
2019-03-03 15:20:44 -0800

    fully implemented meta.py

--- xelib/meta.py
+++ xelib/meta.py
@@ -1,25 +1,39 @@
+from enum import Enum, unique
+
 from xelib.lib import raw_api
-from xelib.helpers import validate
+from xelib.helpers import validate, get_string, get_array
+
+
+@unique
+class SortBy(Enum):
+    None_ = 0
+    FormID = 1
+    EditorID = 2
+    Name = 3
 
 
 def initialize(lib_path):
-    raise NotImplementedError
+    raw_api.InitXEdit(lib_path)
 
 
 def finalize():
-    raise NotImplementedError
+    raw_api.CloseXEdit()
 
 
 def get_global(key):
-    raise NotImplementedError
+    return get_string(lambda len_: raw_api.GetGlobal(key, len_),
+                      error_msg=f'GetGlobal failed')
 
 
 def get_globals():
-    raise NotImplementedError
+    return get_string(lambda len_: raw_api.GetGlobals(len_),
+                      error_msg=f'GetGlobals failed')
 
 
 def set_sort_mode(mode, reverse):
-    raise NotImplementedError
+    validate(raw_api.SetSortMode(mode.value, reverse),
+             f'Failed to set sort mode to {mode} '
+             f'{"ASC" if reverse else "DESC"}')
 
 
 def release(id_):
@@ -27,21 +41,23 @@ def release(id_):
 
 
 def release_nodes(id_):
-    raise NotImplementedError
+    validate(raw_api.ReleaseNodes(id_), f'Failed to release nodes {id_}')
 
 
 def switch(id_, id2):
-    raise NotImplementedError
+    validate(raw_api.Switch(id_, id2),
+             f'Failed to switch interface #{id_} and #{id2}')
 
 
 def get_duplicate_handles(id_):
-    raise NotImplementedError
+    return get_array(lambda len_: raw_api.GetDuplicateHandles(id_, len_),
+                     error_msg=f'Failed to get duplicate handles for {id_}')
 
 
 def clean_store():
-    raise NotImplementedError
+    validate(raw_api.CleanStore(), f'Failed to clean interface store')
 
 
 def reset_store():
-    raise NotImplementedError
+    validate(raw_api.ResetStore(), f'Failed to reset interface store')
 
