0ca7b8afb9150a0403eb62d0a11e37719a4e893e
leontristain <ltristain@yahoo.com>
2019-03-12 23:32:07 -0700

    started on a high level api

---
+++ xelib/xedit.py
@@ -0,0 +1,80 @@
+import os
+import time
+
+from xelib.xelib import Xelib
+
+
+class XEditPlugin:
+    def __init__(self, xelib, handle):
+        self.xelib = xelib
+        self.handle = handle
+
+    @property
+    def name(self):
+        return self.xelib.name(self.handle)
+
+    @property
+    def author(self):
+        return self.xelib.get_file_author(self.handle)
+
+    @author.setter
+    def author(self, value):
+        self.xelib.set_file_author(self.handle, value)
+
+    @property
+    def description(self):
+        return self.xelib.get_description(self.handle)
+
+    @description.setter
+    def description(self, value):
+        self.xelib.set_description(self.handle, value)
+
+    @property
+    def is_esm(self):
+        return self.xelib.get_is_esm(self.handle)
+
+    @is_esm.setter
+    def is_esm(self, value):
+        self.xelib.set_is_esm(self.handle, value)
+
+    @property
+    def next_object_id(self):
+        return self.xelib.get_next_object_id(self.handle)
+
+    @next_object_id.setter
+    def next_object_id(self, value):
+        self.xelib.set_next_object_id(self.handle, value)
+
+    def save(self):
+        return self.xelib.save_file(self.handle)
+
+    def save_as(self, file_path):
+        return self.xelib.save_file(self.handle, file_path=file_path)
+
+
+class XEdit:
+    def __init__(self, game_mode, game_path, plugins):
+        self.game_mode = game_mode
+        self.data_path = game_path
+        self.plugins = plugins
+        self.xelib = Xelib()
+
+    def session(self):
+        class XeditSession:
+            def __init__(xedit):
+                self.xedit = xedit
+
+            def __enter__(self):
+                self.xedit.xelib.__enter__()
+                self.xedit.xelib.set_game_mode(self.game_mode)
+                self.xedit.xelib.set_game_path(self.game_path)
+                self.xedit.xelib.load_plugins(os.linesep.join(self.plugins))
+                while (self.xedit.xelib.get_loader_status() ==
+                           Xelib.LoaderStates.lsActive):
+                    time.sleep(0.1)
+                return self.xedit
+
+            def __exit__(self, exc_type, exc_value, traceback):
+                self.xedit.xelib.__exit__(exc_type, exc_value, traceback)
+
+        return XeditSession(self)
---
+++ xelib/xelib.py
@@ -0,0 +1,110 @@
+import ctypes
+from contextlib import contextmanager
+from ctypes import wintypes
+from pathlib import Path
+
+from xelib.definitions import DelphiTypes, XEditLibSignatures
+from xelib.wrapper_methods.element_values import ElementValuesMethods
+from xelib.wrapper_methods.elements import ElementsMethods
+from xelib.wrapper_methods.errors import ErrorsMethods
+from xelib.wrapper_methods.file_values import FileValuesMethods
+from xelib.wrapper_methods.files import FilesMethods
+from xelib.wrapper_methods.filter import FilterMethods
+from xelib.wrapper_methods.groups import GroupsMethods
+from xelib.wrapper_methods.helpers import HelpersMethods, XelibError
+from xelib.wrapper_methods.masters import MastersMethods
+from xelib.wrapper_methods.messages import MessagesMethods
+from xelib.wrapper_methods.meta import MetaMethods
+from xelib.wrapper_methods.record_values import RecordValuesMethods
+from xelib.wrapper_methods.records import RecordsMethods
+from xelib.wrapper_methods.resources import ResourcesMethods
+from xelib.wrapper_methods.serialization import SerializationMethods
+from xelib.wrapper_methods.setup import SetupMethods
+
+__all__ = ['DLL_PATH', 'Xelib', 'XelibError']
+
+DLL_PATH = Path(__file__).parent / '../XEditLib/XEditLib.dll'
+
+
+class Xelib(ElementValuesMethods,
+            ElementsMethods,
+            ErrorsMethods,
+            FileValuesMethods,
+            FilesMethods,
+            FilterMethods,
+            GroupsMethods,
+            HelpersMethods,
+            MastersMethods,
+            MessagesMethods,
+            MetaMethods,
+            RecordValuesMethods,
+            RecordsMethods,
+            ResourcesMethods,
+            SerializationMethods,
+            SetupMethods):
+
+    def __init__(self):
+        self.dll_path = DLL_PATH
+        self._raw_api = None
+        self._handles_stack = []
+        self._current_handles = set()
+
+    def __enter__(self):
+        if not self._raw_api:
+            self._raw_api = self.load_lib(self.dll_path)
+            self.initialize()
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        if self._raw_api:
+            self.release_handles()
+            self.finalize()
+            kernel32 = ctypes.WinDLL('kernel32', use_last_error=True)
+            kernel32.FreeLibrary.argtypes = [wintypes.HMODULE]
+            kernel32.FreeLibrary(self._raw_api._handle)
+            self._raw_api = None
+
+    def track_handle(self, handle):
+        self._current_handles.add(handle)
+
+    def release_handles(self):
+        for handle in self._current_handles:
+            try:
+                self.release(handle)
+            except XelibError:
+                pass
+
+    @contextmanager
+    def manage_handles(self):
+        try:
+            self._handles_stack.append(self._current_handles)
+            self._current_handles = set()
+            yield
+        finally:
+            self.release_handles()
+            self._current_handles = self._handles_stack.pop()
+
+    @property
+    def raw_api(self):
+        if not self._raw_api:
+            raise XelibError(f'Must use Xelib within its own context; the '
+                             f'code should look something like: `with xelib '
+                             f'as xelib: xelib.do_something`')
+        return self._raw_api
+
+    @staticmethod
+    def load_lib(dll_path):
+        lib = ctypes.CDLL(str(dll_path))
+        types_mapping = {mapping.name: mapping.value for mapping in DelphiTypes}
+        for signature in XEditLibSignatures:
+            method_name = signature.name
+            params, return_type = signature.value
+            try:
+                method = getattr(lib, method_name)
+                method.argtypes = [types_mapping[delphi_type]
+                                   for _, delphi_type in params.items()]
+                if return_type:
+                    method.restype = types_mapping[return_type]
+            except AttributeError:
+                print(f'WARNING: missing function {method_name}')
+        return lib
