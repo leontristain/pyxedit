57cfc566209e75641d4ff6dc66bb243d4914aa60
leontristain <ltristain@yahoo.com>
2019-03-12 21:29:46 -0700

    implemented test_resources.py

---
+++ test/test_resources.py
@@ -0,0 +1,30 @@
+from pathlib import Path
+import pytest
+
+from xelib import XelibError
+
+from . fixtures import xelib  # NOQA: for pytest
+
+
+class TestResources:
+    def test_get_container_files(self, xelib):
+        # should fail if container is not loaded
+        with pytest.raises(XelibError):
+            xelib.get_container_files('blah', '')
+
+        # should return files in container
+        p = str(Path(xelib.get_global('DataPath'), 'Skyrim - Shaders.bsa'))
+        assert len(xelib.get_container_files(p, '')) == 122
+
+        # should filter properly
+        p = str(Path(xelib.get_global('DataPath'), 'Skyrim - Textures.bsa'))
+        assert len(xelib.get_container_files(p, 'textures\\sky\\')) == 47
+
+    def test_get_texture_data(self, xelib):
+        pytest.skip('this method is not yet implemented on my side')
+        # should fail if the resource does not exist
+        # with pytest.raises(XelibError):
+        #     xelib.get_texture_data('abcdefghijk')
+
+        # should return correct bitmap if resource exists
+        # w, h = xelib.get_texture_data('textures\\sky\\skyrimclouds01.dds')
