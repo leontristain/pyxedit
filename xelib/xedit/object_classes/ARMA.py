080b7bf0d88ef5bad23e7f38ba136258bbf246b1
leontristain <ltristain@yahoo.com>
2019-03-20 22:06:24 -0700

    implemented ARMA and ARMO records

--- xelib/xedit/object_classes/ARMA.py
+++ xelib/xedit/object_classes/ARMA.py
@@ -1,5 +1,22 @@
-from xelib.xedit.base import XEditGenericObject
+from xelib.xedit.base import XEditAttribute, XEditGenericObject
 
 
 class XEditArmature(XEditGenericObject):
     SIGNATURE = 'ARMA'
+
+    bodt = body_template_12byte = XEditAttribute('BODT', read_only=True)
+    bod2 = body_template = XEditAttribute('BOD2', read_only=True)
+    rnam = race = XEditAttribute('RNAM')
+    dnam = data = XEditAttribute('DNAM')
+    mod2 = male_model = XEditAttribute('MOD2', read_only=True)
+    mod3 = male_firstperson_model = XEditAttribute('MOD3', read_only=True)
+    mod4 = female_model = XEditAttribute('MOD4', read_only=True)
+    mod5 = female_firstperson_model = XEditAttribute('MOD5', read_only=True)
+    nam0 = base_male_texture = male_skin_texture = XEditAttribute('NAM0')
+    nam1 = base_female_texture = female_skin_texture = XEditAttribute('NAM1')
+    nam2 = base_male_firstperson_texture = XEditAttribute('NAM2')
+    nam3 = base_female_firstperson_texture = XEditAttribute('NAM3')
+    modl = additional_races = included_races = XEditAttribute(
+                                                   'MODL', read_only=True)
+    sndd = footstep_sound = XEditAttribute('SNDD')
+    onam = art_object = XEditAttribute('ONAM')
--- xelib/xedit/object_classes/ARMO.py
+++ xelib/xedit/object_classes/ARMO.py
@@ -4,7 +4,37 @@ from xelib.xedit.base import XEditAttribute, XEditGenericObject
 class XEditArmor(XEditGenericObject):
     SIGNATURE = 'ARMO'
 
+    vmad = script_info = XEditAttribute('VMAD', read_only=True)
     obnd = object_bounds = XEditAttribute('OBND', read_only=True)
     full = full_name = XEditAttribute('FULL')
     eitm = enchantment = object_effect = XEditAttribute('EITM')
     eamt = enchantment_amount = XEditAttribute('EAMT')
+
+    mod2 = male_model = XEditAttribute('MOD2', read_only=True)
+    icon = male_inventory_image = XEditAttribute('ICON')
+    mico = male_message_image = XEditAttribute('MICO')
+    mod4 = female_model = XEditAttribute('MOD4', read_only=True)
+    ico2 = female_inventory_image = XEditAttribute('ICO2')
+    mic2 = female_message_image = XEditAttribute('MIC2')
+
+    bodt = body_template_12byte = XEditAttribute('BODT', read_only=True)
+    bod2 = body_template = XEditAttribute('BOD2', read_only=True)
+    dest = destructible = destruction_data = XEditAttribute('DEST')
+
+    ynam = pickup_sound = XEditAttribute('YNAM')
+    znam = drop_sound = XEditAttribute('ZNAM')
+
+    bmct = ragdoll_constraint_template = XEditAttribute('BMCT')
+    etyp = equipment_type = equipment_slot = XEditAttribute('ETYP')
+    bids = bash_impact_data_set = XEditAttribute('BIDS')
+    bamt = bash_material = alternate_block_material = XEditAttribute('BAMT')
+    rnam = race = XEditAttribute('RNAM')
+
+    ksiz = keyword_count = XEditAttribute('KSIZ')
+    kwda = keywords = XEditAttribute('KWDA')
+
+    desc = description = XEditAttribute('DESC')
+    modl = armature = XEditAttribute('MODL', read_only=True)
+    data = XEditAttribute('DATA', read_only=True)
+    dnam = armor_rating = XEditAttribute('DNAM')
+    tnam = template = template_armor = XEditAttribute('TNAM')
