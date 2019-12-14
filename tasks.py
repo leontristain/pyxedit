748be0bbea5a76a33aaf9e975e6fe77b2386dff6
leontristain <ltristain@yahoo.com>
2019-12-14 13:19:49 -0800

    added invoke wrapper for tests

--- tasks.py
+++ tasks.py
@@ -2,5 +2,10 @@ from invoke import task
 
 
 @task
+def test(c, test='test'):
+    c.run(f'python -m pytest -v {test}')
+
+
+@task
 def build_docs(c):
     c.run('sphinx-build -b html docs docs/_build')
